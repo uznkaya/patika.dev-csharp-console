5a4910890a874e4cbcda685ad677db078288d61f
Mehmet Can Uzunkaya <105919067+uznkaya@users.noreply.github.com>
2023-02-27 03:05:52 +0300

    Add files via upload

---
+++ switchcase/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace switchcase;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int  ay = DateTime.Now.Month;
+        switch (ay)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Winter.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring.");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer.");
+                    break;
+
+                default:
+                    Console.WriteLine("Autumn");
+                    break;
+            }
+
+    }
+}
