cd103d2a1c6557a6b941538e3a96204c03ab9fb4
Mehmet Can Uzunkaya <105919067+uznkaya@users.noreply.github.com>
2023-02-27 03:32:19 +0300

    Add files via upload

---
+++ forbreakcontinue/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace forbreakcontinue;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i=1;i<10;i++){
+            if(i==4){
+                break;
+            }
+            Console.WriteLine(i);
+        }
+        for(int i=1;i<10;i++){
+            if(i==4){
+                continue;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
