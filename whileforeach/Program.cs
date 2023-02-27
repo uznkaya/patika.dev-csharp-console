e9041feae17226a9163c053bd9e3d2c396635cd3
Mehmet Can Uzunkaya <105919067+uznkaya@users.noreply.github.com>
2023-02-27 03:41:32 +0300

    Add files via upload

---
+++ whileforeach/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace whileforeach;
+class Program
+{
+    static void Main(string[] args)
+    {
+        char character = 'a';
+        while(character<'z'){
+            Console.Write(character);
+            character++;
+        }
+        Console.WriteLine();
+        string[] arabalar = {"Mercedes","Toyota","Ferrari"};
+        foreach(var isim in arabalar){
+            Console.WriteLine(isim);
+        }
+    }
+}
