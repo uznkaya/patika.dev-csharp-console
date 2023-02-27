cfa34b35662a6edb8786046943b5f8953d3b01d2
Mehmet Can Uzunkaya <105919067+uznkaya@users.noreply.github.com>
2023-02-27 03:05:03 +0300

    Add files via upload

---
+++ ifelse/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ifelse;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+        if(time >=6 && time <11){
+            Console.WriteLine("Gunaydin!");
+        }
+        else if(time <=18){
+            Console.WriteLine("Iyi Gunler!");
+        }
+        else{
+            Console.WriteLine("Iyı Geceler!");
+        }
+        string sonuc = time<=18 ? "Iyi Gunler!" : "Iyi Geceler!";
+        sonuc = time>=6 && time<11 ? "Gunaydin!" : time <=18 ? "Iyi Gunler!": "Iyi Geceler!";
+        Console.WriteLine(sonuc);
+    }
+}
