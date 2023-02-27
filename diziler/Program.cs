5e024204ecc4473f564302f593f6c8c2d0763ede
Mehmet Can Uzunkaya <105919067+uznkaya@users.noreply.github.com>
2023-02-27 03:54:25 +0300

    Add files via upload

---
+++ diziler/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace diziler;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] hayvanlar = {"Kedi","Kopek","Geyik"};
+        Console.WriteLine(hayvanlar[2]);
+
+        int[] sayilar = {2,4,8,0,5,2};
+        int toplam = 0;
+        foreach(var item in sayilar){
+            toplam += item;
+        }
+        Console.WriteLine("Sayilar Dizisi Ortalamasi :"+Convert.ToDouble(toplam)/sayilar.Length);
+    }
+}
